44b3dc1bd63b65fcae2c8cea0caaaf05a6ea6f67
Jón Levy <levy@andes.is>
2024-03-26 07:31:30 +0000

    fix: split up stufF

---
+++ lua/custom/plugins/neotree.lua
@@ -0,0 +1,46 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  opts = {
+    sources = { 'filesystem', 'buffers', 'git_status', 'document_symbols' },
+    open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+    },
+    window = {
+      mappings = {
+        ['<space>'] = 'none',
+        ['Y'] = {
+          function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            vim.fn.setreg('+', path, 'c')
+          end,
+          desc = 'copy path to clipboard',
+        },
+        ['O'] = {
+          function(state)
+            require('lazy.util').open(state.tree:get_node().path, { system = true })
+          end,
+          desc = 'open with system application',
+        },
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = '',
+        expander_expanded = '',
+        expander_highlight = 'NeoTreeExpander',
+      },
+    },
+  },
+}
